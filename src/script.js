9ce1dc9b077ac9b3c463cfafc55f0bb7cf0fd24d
Krizhinka <127995890+Krizhinka@users.noreply.github.com>
2023-04-13 23:59:50 +0300

    Displayed the forecast data using the API

--- src/script.js
+++ src/script.js
@@ -27,6 +27,12 @@ let currentDayTime = document.querySelector("#current-day-time");
 
 currentDayTime.innerHTML = `${day}  ${hours}:${minutes}`;
 
+function search(city) {
+  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
 function showCityTemperature(event) {
   event.preventDefault();
 
@@ -70,6 +76,8 @@ function showTemperature(response) {
       "src",
       `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
+
+  getForecast(response.data.coord);
 }
 
 function showPosition(position) {
@@ -103,6 +111,8 @@ function displayFahrenheitTemperature(event) {
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
+let celsiusTemperature = null;
+
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
@@ -118,3 +128,65 @@ function displayCelsiusTemperature(event) {
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "c819171fe0abdc14039af4ef5dda283b";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let day = date.getDay();
+
+  let days = ["Sun", "Mon", "Tue", "WEd", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+                        <div class="weather-forecast-date">
+                            ${formatDay(forecastDay.dt)}
+                        </div>
+                        <img src="https://openweathermap.org/img/wn//${
+                          forecastDay.weather[0].icon
+                        }@2x.png" alt="" width="80" />
+                        <div class="weather-forecast-temperatures">
+                            <span class="weather-forecast-temperature-min">${Math.round(
+                              forecastDay.temp.min
+                            )}° </span>
+                            <span class="weather-forecast-temperature-max">${Math.round(
+                              forecastDay.temp.max
+                            )}° </span>
+                        </div>
+                    </div>
+       `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+  console.log(forecastHTML);
+}
+
+
+search("kyiv");
